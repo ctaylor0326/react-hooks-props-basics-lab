e7cef85b922e06d45299ca22620d2094b446677d
CahenTaylor <cahentaylor@gmail.com>
2023-04-17 14:49:19 -0600

    all tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,15 @@ import About from "./About";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
-
+const { name, city, bio, color, links } = user
+// try this instead and pass props here:
+const {github, linkedin} = links
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} city= {city} color={color}/>
+      <About bio={bio} github={github} linkedin={linkedin}/>
     </div>
   );
 }
